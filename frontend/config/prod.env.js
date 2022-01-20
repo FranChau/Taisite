0566ad2d68b0ba245a7bd3b92cba7f6e4114ed59
zoufan <154188917@qq.com>
2022-01-20 18:45:58 +0800

    first commit

---
+++ frontend/config/prod.env.js
@@ -0,0 +1,6 @@
+'use strict'
+module.exports = {
+  NODE_ENV: '"production"',
+  BASE_API: '"http://192.168.0.217:5050"',
+  CASE_IMPORT_URI: '"/api/importTestCases"'
+}
---
+++ frontend/src/api/testReport.js
@@ -0,0 +1,38 @@
+import request from '@/utils/request'
+
+export function getReportList (project_id, params, header) {
+  return request({
+    url: `/api/project/${project_id}/reportsList`,
+    headers: header,
+    params: params,
+    method: 'GET'
+  })
+}
+
+export function getReportDetail (project_id, report_id, header) {
+  return request({
+    url: `/api/project/${project_id}/reportsList/${report_id}`,
+    headers: header,
+    params: null,
+    method: 'GET'
+  })
+}
+
+export function exportReportDetail (project_id, report_id, header) {
+  return request({
+    url: `/api/project/${project_id}/reportsList/${report_id}/export`,
+    headers: header,
+    responseType: 'blob',
+    method: 'POST',
+    data: null
+  })
+}
+
+export function removeReport (report_ids, header) {
+  return request({
+    url: `/api/project/delateReport`,
+    headers: header,
+    method: 'POST',
+    data: report_ids
+  })
+}
